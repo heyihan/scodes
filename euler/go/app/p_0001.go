1b17a3a2c5729f3b37a68649bf31f90f72f1be15
heyihan <heyihan@didichuxing.com>
2019-05-20 23:32:25 +0800

    add go sources

--- euler/go/app/p_0001.go
+++ euler/go/app/p_0001.go
@@ -8,8 +8,11 @@ import (
 
 func main() {
 
-    sum1 := mymath.SumOfSeries(3, 10)
-    fmt.Println("sum ", sum1)
+    sum1 := mymath.SumOfSeries(3, 999)
+    sum2 := mymath.SumOfSeries(5, 999)
+    sum3 := mymath.SumOfSeries(3*5, 999)
+
+    fmt.Printf("%d + %d - %d = %d\n", sum1, sum2, sum3, sum1+sum2-sum3)
 
 
     os.Exit(0)
